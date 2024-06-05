1b0d26abc28d7b01c317d5239cace9fb07c94b3c
osmany34 <127791332+osmany34@users.noreply.github.com>
2024-06-05 17:11:40 +0300

    Pratik - Dosyadan Sayıları Okuma ve Toplama

---
+++ src/Week6/FileNumbers.java
@@ -0,0 +1,23 @@
+package Week6;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileNumbers {
+    public static void main(String[] args) {
+        String fileName = "C:\\javademos\\numbers.txt"; // Dosya adını burada belirtin
+        int sum = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                int number = Integer.parseInt(line);
+                sum += number;
+            }
+        } catch (IOException e) {
+            System.err.println("Dosya okuma hatası: " + e.getMessage());
+        }
+
+        System.out.println("Toplam: " + sum);
+    }
+}
